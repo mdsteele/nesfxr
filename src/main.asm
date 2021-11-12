a8153ce3b85bb4442ed1776ddb98e9abc441b8a4
Matthew D. Steele <mdsteele@alum.mit.edu>
2021-11-12 16:49:26 -0500

    Implement changing volume and period values

--- src/main.asm
+++ src/main.asm
@@ -28,7 +28,12 @@ DUTY_1_2 = %10
 DUTY_3_4 = %11
 MAX_DUTY = %11
 
+MAX_VOLUME = $f
+MAX_PERIOD = $3ff
+
 PPUADDR_DUTY_VALUE = (PPUADDR_NAME0 + SCREEN_WIDTH_TILES * (MENU_TOP_ROW + CURSOR_ROW_DUTY) + MENU_LEFT_COL + MENU_LABEL_COLS)
+PPUADDR_VOLUME_VALUE = (PPUADDR_NAME0 + SCREEN_WIDTH_TILES * (MENU_TOP_ROW + CURSOR_ROW_VOLUME) + MENU_LEFT_COL + MENU_LABEL_COLS + 1)
+PPUADDR_PERIOD_VALUE = (PPUADDR_NAME0 + SCREEN_WIDTH_TILES * (MENU_TOP_ROW + CURSOR_ROW_PERIOD) + MENU_LEFT_COL + MENU_LABEL_COLS + 1)
 
 ;;;=========================================================================;;;
 
@@ -110,6 +115,18 @@ Data_Palettes_end:
 
 .CODE
 
+;;; @param a The hex digit, from 0-F.
+;;; @return a The ASCII value.
+.PROC Func_HexDigitToAscii
+    cmp #$a
+    bge @letter
+    add #'0'
+    rts
+    @letter:
+    add #('A' - 10)
+    rts
+.ENDPROC
+
 .PROC Func_UpdateDuty
     ldx Zp_PpuTransferLen_u8
     lda #3
@@ -158,6 +175,64 @@ Data_Palettes_end:
     rts
 .ENDPROC
 
+.PROC Func_UpdateVolume
+    ldx Zp_PpuTransferLen_u8
+    lda #1
+    sta Ram_PpuTransfer_start, x
+    inx
+    lda #>PPUADDR_VOLUME_VALUE
+    sta Ram_PpuTransfer_start, x
+    inx
+    lda #<PPUADDR_VOLUME_VALUE
+    sta Ram_PpuTransfer_start, x
+    inx
+    ;; Buffer first (only) digit:
+    lda Ram_Volume_u8
+    jsr Func_HexDigitToAscii
+    sta Ram_PpuTransfer_start, x
+    inx
+    stx Zp_PpuTransferLen_u8
+    ;; Update audio register:
+    ;; TODO
+    rts
+.ENDPROC
+
+.PROC Func_UpdatePeriod
+    ldx Zp_PpuTransferLen_u8
+    lda #3
+    sta Ram_PpuTransfer_start, x
+    inx
+    lda #>PPUADDR_PERIOD_VALUE
+    sta Ram_PpuTransfer_start, x
+    inx
+    lda #<PPUADDR_PERIOD_VALUE
+    sta Ram_PpuTransfer_start, x
+    inx
+    ;; Buffer first digit:
+    lda Ram_Period_u16 + 1
+    jsr Func_HexDigitToAscii
+    sta Ram_PpuTransfer_start, x
+    inx
+    ;; Buffer second digit:
+    lda Ram_Period_u16 + 0
+    .repeat 4
+    lsr a
+    .endrepeat
+    jsr Func_HexDigitToAscii
+    sta Ram_PpuTransfer_start, x
+    inx
+    ;; Buffer third digit:
+    lda Ram_Period_u16 + 0
+    and #$0f
+    jsr Func_HexDigitToAscii
+    sta Ram_PpuTransfer_start, x
+    inx
+    stx Zp_PpuTransferLen_u8
+    ;; Update audio registers:
+    ;; TODO
+    rts
+.ENDPROC
+
 .PROC Func_IncrementDuty
     lda Ram_Duty_u8
     cmp #MAX_DUTY
@@ -170,13 +245,55 @@ Data_Palettes_end:
 .ENDPROC
 
 .PROC Func_IncrementVolume
-    ;; TODO
+    lda Ram_Volume_u8
+    cmp #MAX_VOLUME
+    blt @increment
     rts
+    @increment:
+    add #1
+    sta Ram_Volume_u8
+    jmp Func_UpdateVolume
 .ENDPROC
 
 .PROC Func_IncrementPeriod
-    ;; TODO
-    rts
+    ;; Store increment amount in XY.
+    lda Zp_P1ButtonsHeld_u8
+    and #BUTTON_A
+    beq @not100
+    ldx #$01
+    ldy #$00
+    beq @increment  ; unconditional
+    @not100:
+    lda Zp_P1ButtonsHeld_u8
+    and #BUTTON_B
+    beq @not10
+    ldx #$00
+    ldy #$10
+    bne @increment  ; unconditional
+    @not10:
+    ldx #$00
+    ldy #$01
+    ;; Add XY to Ram_Period_u16.
+    @increment:
+    tya
+    add Ram_Period_u16 + 0
+    sta Ram_Period_u16 + 0
+    txa
+    adc Ram_Period_u16 + 1
+    sta Ram_Period_u16 + 1
+    ;; Clamp Ram_Period_u16 to MAX_PERIOD.
+    lda Ram_Period_u16 + 1
+    cmp #>(MAX_PERIOD + 1)
+    blt @noClamp
+    lda Ram_Period_u16 + 0
+    cmp #<(MAX_PERIOD + 1)
+    blt @noClamp
+    lda #>MAX_PERIOD
+    sta Ram_Period_u16 + 1
+    lda #<MAX_PERIOD
+    sta Ram_Period_u16 + 0
+    @noClamp:
+    jmp Func_UpdatePeriod
 .ENDPROC
 
 .PROC Func_IncrementCurrentRow
@@ -201,13 +318,57 @@ Data_Palettes_end:
 .ENDPROC
 
 .PROC Func_DecrementVolume
-    ;; TODO
+    lda Ram_Volume_u8
+    bne @decrement
     rts
+    @decrement:
+    sub #1
+    sta Ram_Volume_u8
+    jmp Func_UpdateVolume
 .ENDPROC
 
 .PROC Func_DecrementPeriod
-    ;; TODO
-    rts
+    ;; Store decrement amount in XY.
+    lda Zp_P1ButtonsHeld_u8
+    and #BUTTON_A
+    beq @not100
+    ldx #$01
+    ldy #$00
+    beq @compare  ; unconditional
+    @not100:
+    lda Zp_P1ButtonsHeld_u8
+    and #BUTTON_B
+    beq @not10
+    ldx #$00
+    ldy #$10
+    bne @compare  ; unconditional
+    @not10:
+    ldx #$00
+    ldy #$01
+    ;; If XY >= Ram_Period_u16, set Ram_Period_u16 to zero.
+    @compare:
+    txa
+    cmp Ram_Period_u16 + 1
+    blt @noClamp
+    tya
+    cmp Ram_Period_u16 + 0
+    blt @noClamp
+    lda #0
+    sta Ram_Period_u16 + 0
+    sta Ram_Period_u16 + 1
+    jmp Func_UpdatePeriod
+    @noClamp:
+    ;; Subtract XY from Ram_Period_u16.
+    tya
+    eor #$ff
+    sec
+    adc Ram_Period_u16 + 0
+    sta Ram_Period_u16 + 0
+    txa
+    eor #$ff
+    adc Ram_Period_u16 + 1
+    sta Ram_Period_u16 + 1
+    jmp Func_UpdatePeriod
 .ENDPROC
 
 .PROC Func_DecrementCurrentRow
